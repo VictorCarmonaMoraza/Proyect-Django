09ba147351dd26b4a8b3f444569ac2394220fedf
Victor <VictorCarmona9035@hotmail.com>
2023-05-01 20:21:43 +0200

    15-Agregar un nuevo Objeto de Modelo con Django(518) - parte 1

--- sap/personas/views.py
+++ sap/personas/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, get_object_or_404
+from django.forms import modelform_factory
 
 from personas.models import Persona
+
 
 # Create your views here.
 def detallePersona(request, id):
     # Recuperamos la persona mediante el id de nuestra BBDD
-    #persona = Persona.objects.get(pk=id)
-    persona = get_object_or_404(Persona,pk=id)
+    # persona = Persona.objects.get(pk=id)
+    persona = get_object_or_404(Persona, pk=id)
     return render(request, 'personas/detalle.html', {'persona': persona})
+
+PersonaForm = modelform_factory(Persona, exclude=[])
+
+def nuevaPersona(request):
+    formaPersona = PersonaForm()
+    return render(request, 'personas/nuevo.html', {'formaPersona': formaPersona})
